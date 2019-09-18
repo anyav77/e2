37466a022ba613f41d861425dfeb108f6744873c
Anya Zinoveva <anya@educationalfilms.net>
2019-09-17 17:09:44 -0700

    Updated root readme; added image to practice app

--- practice/index.php
+++ practice/index.php
@@ -1,2 +1,4 @@
 <!doctype html> <html lang='en'> <head> 	<title>Practice Application</title> 	<meta charset='utf-8'> 
-</head> <body> 	<h1>Practice Application</h1> </body> </html>
+</head> <body> 	<h1>Practice Application</h1>
+<img src="/images/hes-logo.png"> 
+</body> </html>
