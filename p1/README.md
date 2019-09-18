798036b2c2d127ad27692729d68dfd864c5664dc
Anya Zinoveva <anya@educationalfilms.net>
2019-09-17 21:42:54 -0700

    Created and Published Project 1 to Github

---
+++ p1/README.md
@@ -0,0 +1,10 @@
+# Project 1
++ By: *Anna Zinoveva*
++ Production URL: <http://p1.atozez.com>
+
+
+## Outside resources
+n/a
+
+## Notes for instructor
+[Project 1](http://p1.atozez.com)
---
+++ p1/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang='en'>
+
+<head>
+
+    <title>John Gosier</title>
+    <meta charset='utf-8'>
+
+</head>
+
+<body>
+
+        <h1>Anya Zinoveva</h1>
+
+        <img src='images/anya-zinoveva-photo.jpg' alt='Photo of Anya Zinoveva'>
+
+        <h2>About Me</h2>
+        <p>
+            Hi, my name is Anya Zinoveva. This is my last course toward the Graduate Certificate in Website 
+Technologies. I'm also starting an application process to the ALM in Digital Media Design.
+        </p>
+        <p>
+            I come from a graphic design/web design background, but I have years of experience working in IT. I'm 
+trying to bridge a gap between education and work experience. I have been looking forward to this class for a long 
+time, and I'm excited about it.
+        </p>
+
+</body>
+
+</html>
