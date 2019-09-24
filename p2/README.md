5e99078b1631649c7eb1e8f89455cf235766f200
Anya Zinoveva <anya@educationalfilms.net>
2019-09-24 16:25:12 -0700

    Started p2; PHP practice files

---
+++ p2/README.md
@@ -0,0 +1,9 @@
+# Project 2
++ By: Anna Zinoveva
++ Production URL: <http://p2.atozez.com>
+
+## Game planning
+
+## Outside resources
+
+## Notes for instructor
---
+++ p2/index.php
@@ -0,0 +1,19 @@
+<?php require "index-controller.php"?>
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+
+    <title>Rock Paper Scissors</title>
+    <meta charset='utf-8'>
+
+</head>
+
+<body>
+    <h1>Rock Paper Scissors</h1>
+
+    <h2>Mechanics</h2>
+
+    <h2>Results</h2>
+
+</body>
+</html>
