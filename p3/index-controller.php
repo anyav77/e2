501b056884a42c21d9eb48f3f3868d3527964aec
Anya Zinoveva <anya@educationalfilms.net>
2019-10-13 18:34:19 -0700

    Project 3 in progress: results NULL show in index.php - var_dump proof

--- p3/index-controller.php
+++ p3/index-controller.php
@@ -2,11 +2,11 @@
 
 session_start();
 
+var_dump($_SESSION['results']);
 if (isset($_SESSION['results'])){
     $results = $_SESSION['results'];
     $showResults = true;
     $_SESSION['results'] = null;
 }else{
-    var_dump($_SESSION['results']);
     $showResults = false;
 }
