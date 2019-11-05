d734a1a12bf9bd459505d0ded799aa82e5e06e28
Anya Zinoveva <anya@educationalfilms.net>
2019-11-05 15:21:09 -0800

    Week 8 final game based on the package

--- practice/week8/index-controller.php
+++ practice/week8/index-controller.php
@@ -1,30 +1,16 @@
 <?php
-
-// require "Number.php";
-// require "EvenNumber.php";
-// require "Debug.php";
-
-// use E2\Number;
-// use E2\EvenNumber;
-// use E2\Debug;
-
-// $example1 = new Number(21);
-// // var_dump($example1->getHalf());
-// var_dump($example1->isValid());
-// var_dump($example1->num);
-
-// $example2 = new EvenNumber(21);
-// //var_dump($example2->getHalf());
-// var_dump($example2->isValid());
-// var_dump($example1->num);
+session_start();
 
 require __DIR__.'/vendor/autoload.php';
 
-use RPS\Game;
+use App\Debug;
 
-$game = new Game();
+# Define array of moves - used in the view to display each move
+$moves = ['rock', 'paper', 'scissors'];
 
-# Each invocation of the `play` method will play and track a new round of player (given move) vs. computer
-$game->play('rock');
+# Extract the results from the session if they exist
+$lastGame = $_SESSION['results']['lastGame'] ?? null;
+$recentGames = $_SESSION['results']['recentGames'] ?? null;
 
-App\Debug::dump(['a', 'b', ['x', 'y', 'z']]);
+# Clear the results of the last game
+$_SESSION['results']['lastGame'] = null;
